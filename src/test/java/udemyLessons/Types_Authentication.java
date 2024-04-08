ef593c5db08866e19a6f348f99f4d13785b6a2f0
sabareesan <sabareesan.j@necsws.com>
2024-04-08 11:41:55 +0530

    Authentication types- Updates'

--- src/test/java/udemyLessons/Types_Authentication.java
+++ src/test/java/udemyLessons/Types_Authentication.java
@@ -42,7 +42,11 @@ public class Types_Authentication {
 	}
 	
 	@Test(priority = 3)
-	public void bearerToken() {
+	public void bearerTokenAuthentication() {
+		
+		/** client(User_Web) first authenticates itself with the server using credentials like a username and password. 
+		Once authenticated, the server issues a bearer token to the client **/
+		
 		String bearerToken = "(check the docs fetched from Udemy folder for the token)"; // Generated from Github acc, but it will be different for each applications
 		
 		given().headers("Authorization", "Bearer "+ bearerToken)
@@ -50,5 +54,37 @@ public class Types_Authentication {
 		.then().statusCode(200).log().all();
 		
 	}
+	
+	@Test(priority = 4)
+	public void oauth1Authentication() {
+		
+		given()
+		// For the below parameters we need to contact and get it from the Devs
+		 .auth().oauth("consumerKey", "consumerSecret", "accessToken", "tokenSecret")
+		.when().get("URL")
+		.then().statusCode(200).log().all();
+	}
+	
+	@Test(priority = 5)
+	public void oauth2Authentication() {
+		/** In oauth2 Authentication, we need to hit a request and fetch some data from its response/URL,
+		Then we need to use that specific data as input in another request, by which we can get the accesstoken
+		Post that we can use/input the accesstoken in another request**/  
+		// ==>Check "OAuthAndDeSerialization" Class
+		
+		given()
+		 .auth().oauth2("accessToken")
+		.when().get("URL")
+		.then().statusCode(200).log().all();
+	}
 
+	@Test(priority = 6)
+	public void apiKeyAuthentication() {
+		// To generate an API key, we need to login the api_url and we can generate it.
+		
+		given().queryParam("appid", "fdasdge5hdrtw123414xsf")// app id is the API key
+				.given().get("URL")
+				.then().statusCode(200).log().all();
+	}
 }
+
